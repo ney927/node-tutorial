bed4e88046c5e47b47153a70b7917df248f47203
ney927 <nbilling9@yahoo.com>
2022-12-30 19:31:43 -0500

    using express

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.listen(3000, ()=>{console.log('listening')});
 
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    const fillerBlogs = [
+        {title: 'Blog 1', snippet: '1live love laugh the day until it fades into the endless dark that is life #lol'},
+        {title: 'Blog 2', snippet: '2live love laugh the day until it fades into the endless dark that is life #lol'},
+        {title: 'Blog 3', snippet: '3live love laugh the day until it fades into the endless dark that is life #lol'},
+    ];
+
+    res.render('index', {title: 'HomePage', blogs: fillerBlogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
 });
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create Blog'});
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {title: '404'});
 });
